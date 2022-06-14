ea931c93470733ac564f17baeb30f74ddb89523a
innomindsT <107095471+innomindsT@users.noreply.github.com>
2022-06-14 17:12:53 +0530

    Update pascal_triangle.c

--- pascal_triangle.c
+++ pascal_triangle.c
@@ -7,22 +7,22 @@ int main()
 	arr[1]=arr1[1]=1;
 	printf("Enter the number of lines of pascal triangle :");
 	scanf("%d",&num);
-	for (int i=1;i<=num;i++)		//loop for number of lines of pascal triangle
+	for (int i=1;i<=num;i++)					//loop for number of lines of pascal triangle
 	{
-		for (int j=1;j<=i;j++)		//starting the loop for logic to each line
+		for (int j=1;j<=i;j++)					//starting the loop for logic to each line
 		{
-			if (j==1 || j==i)	//Declaring the 0th and ith place of each line equals to 1
+			if (j==1 || j==i)				//Declaring the 0th and ith place of each line equals to 1
 			{
 				arr1[j]=1;
 				printf("%d ",arr1[j]);
 			}
-			else			//Calculating the sum of two elements of the above line
+			else						//Calculating the sum of two elements of the above line
 			{
 				arr1[j]=arr[j-1]+arr[j];
 				printf("%d ",arr1[j]);
 			}
 		}
-		for(int j=1;j<=i;j++)
+		for(int j=1;j<=i;j++)					//equalating arr1 elements to arr
 		{
 			arr[j]=arr1[j];
 		}
